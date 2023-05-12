3beac932bbad2a95f01bf0c560c2487f977acb51
FerranBalleste <fe.ba.solsona@gmail.com>
2023-05-12 15:10:08 +0200

    Listing Tooltip

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,16 @@ import { ref, computed } from 'vue'
 import santiago from "./assets/santiago.json"
 import listing_counts from "./assets/listing_number.json"
 
-// Random data
-let geojson = ref(santiago)
+const legendLabels = [
+  {"id":1, "alias": "N. of listings"},
+  {"id":2, "alias": "XXX"},
+  {"id":3, "alias": "YYY"}
+]
+const selected = ref(legendLabels[0])
 
+// Fill properties values from json
 const featured = computed( () => {
-  return geojson.value.features.map(i => {
+  return santiago.features.map(i => {
       let c = listing_counts.find( elem => elem.cleansed == i.properties.neighbourhood)
       i.properties.count = c.count
       return i
@@ -19,30 +24,44 @@ const featured = computed( () => {
 })
 
 const colors = ["#074cad", "#115f9a", "#1984c5", "#22a7f0", "#48b5c4", "#76c68f", "#a6d75b", "#c9e52f", "#d0ee11", "#d0f400"]
-const legend = [
-  {"value": 10, "color": colors[9]},
-  {"value": 20, "color": colors[8]},
-  {"value": 30, "color": colors[7]},
-  {"value": 40, "color": colors[6]},
-  {"value": 50, "color": colors[5]},
-  {"value": 100, "color": colors[4]},
-  {"value": 500, "color": colors[3]},
-  {"value": 1000, "color": colors[2]},
-  {"value": 2000, "color": colors[1]},
-  {"value": ">2000", "color": colors[0]}
-]
+const legend = computed(() => { 
+  switch(selected.value.id) {
+    case 1:
+      return [
+        {"value": 10, "color": colors[9]},
+        {"value": 20, "color": colors[8]},
+        {"value": 30, "color": colors[7]},
+        {"value": 40, "color": colors[6]},
+        {"value": 50, "color": colors[5]},
+        {"value": 100, "color": colors[4]},
+        {"value": 500, "color": colors[3]},
+        {"value": 1000, "color": colors[2]},
+        {"value": 2000, "color": colors[1]},
+        {"value": ">2000", "color": colors[0]}
+      ]
+    case 2:
+      return [
+        {"value": 10, "color": colors[9]},
+        {"value": 100, "color": colors[8]},
+        {"value": 200, "color": colors[7]},
+        {"value": 300, "color": colors[6]},
+        {"value": 400, "color": colors[5]},
+        {"value": 100, "color": colors[4]},
+        {"value": 500, "color": colors[3]},
+        {"value": 1000, "color": colors[2]},
+        {"value": 2000, "color": colors[1]},
+        {"value": ">2000", "color": colors[0]}
+      ]
+    default:
+      return undefined
+  }
+})
 
 function getColor(value) {
-  if(value < 10) return colors[9]
-  if(value < 20) return colors[8]
-  if(value < 30) return colors[7]
-  if(value < 40) return colors[6]
-  if(value < 50) return colors[5]
-  if(value < 100) return colors[4]
-  if(value < 500) return colors[3]
-  if(value < 1000) return colors[2]
-  if(value < 2000) return colors[1]
-  else return colors[0]
+  for(const legendItem of legend.value.slice(0,9)){
+    if(value < legendItem.value) return legendItem.color
+  }
+  return legend.value?.[9]?.color
 }
 
 function styleFunction(data) {
@@ -58,34 +77,45 @@ function styleFunction(data) {
 
 const zoom = ref(10)
 const center = ref([-33.42, -70.52])
-const show = ref(true)
+
+const options = {
+  onEachFeature: function onEachFeature(feature, layer) {
+    if (feature.properties.count) {
+      layer.bindTooltip(feature.properties.count + " listings");
+    } else {
+      layer.bindTooltip("Not in port");
+    }
+  }
+}
+
 </script>
 
 <template>
   <main>
-    <l-map ref="map" v-model:zoom="zoom" v-model:center="center" :useGlobalLeaflet="false">
-      <LTileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-                    layer-type="base"
-                    name="Stadia Maps Basemap"
+    <select v-model="selected" class="selector">
+        <option :value="opt" v-for="(opt,index) of legendLabels" :key="index">
+          {{ opt.alias }}
+        </option>
+    </select>
+
+    <l-map ref="map" v-model:zoom="zoom" v-model:center="center" :useGlobalLeaflet="false" :key="selected">
+      <LTileLayer name="Stadia Maps Basemap" layer-type="base"
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"      
       >
       </LTileLayer>
-      <LGeoJson
-        v-if="show"
-        :geojson="featured"
-        :optionsStyle="styleFunction"
-      />
+
+      <LGeoJson :geojson="featured" :optionsStyle="styleFunction" :options="options"></LGeoJson>
+
       <LControl position="bottomright" class="legend">
-        <h2>Legend</h2>
-        <h3>N. of listings</h3>
+        <h3>Legend</h3>
+        <h4>{{ selected.alias }}</h4>
         <div v-for="item of legend" style="display:flex; gap:5px;">
           <div class="color" :style="{backgroundColor: item.color}"></div>
           <span>{{ item.value }}</span>
         </div>
-        
       </LControl>
     </l-map>
   </main>
-  
 </template>
 
 <style>
@@ -99,8 +129,23 @@ main {
   width: 100vw;
 }
 
+select{
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  z-index: 500;
+  
+  width: 10rem;
+  padding: 0.5rem 5px;
+  border-radius: 5px;
+}
+
+select:focus{ 
+  outline: 3px solid darkblue;
+}
+
 .legend{
-  width: 150px;
+  width: 130px;
   margin: 2rem;
   padding: 0.5rem 1rem;
   font-weight: bold;
@@ -112,5 +157,4 @@ main {
   width: 40px;
   height: 15px;
 }
-
 </style>
